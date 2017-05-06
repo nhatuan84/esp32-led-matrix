9d1483b448bff75aac0ea98b6285295cb5927def
Tuan <nha.tuan84@gmail.com>
2017-05-06 20:19:23 +0700

    updated examples

---
+++ examples/esp32spi/esp32spi.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include "LedMatrix.h"
+
+
+#define NUMBER_OF_DEVICES 3 //number of led matrix connect in series
+#define CS_PIN 15
+#define CLK_PIN 14
+#define MISO_PIN 2 //we do not use this pin just fill to match constructor
+#define MOSI_PIN 12
+
+LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
+  
+void setup() {
+  ledMatrix.init();
+  ledMatrix.setText("EasyIoT");
+}
+
+void loop() {
+  ledMatrix.clear();
+  ledMatrix.scrollTextLeft();
+  ledMatrix.drawText();
+  ledMatrix.commit();
+  delay(50);
+}
